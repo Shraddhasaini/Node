81479e02a6f2d76a167c8ea5b9ecc0bac8c4edb0
Shraddha saini <shraddhasaini99@gmail.com>
2020-05-05 12:15:12 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' },
+];
+
 app.get('/', (req, res) => {
   res.send('Hello World!! Hello Shraddha');
 });
 //app.post()
 //app.put()
 //app.delete()
-app.get('/api/courses',(req,res) => {
-  res.send([1,2,3]);
+app.get('/api/courses/:id ',(req,res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('No course ID found');  //404
+  res.send(course);
 });
 
 // /api/courses/1
-app.get('/api/posts/:year/:month', (req ,res) => {
+app.get('/api/courses/:id', (req ,res) => {
   res.send(req.query);
 });
 
